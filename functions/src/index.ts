53a878f6d6f42d0d51dbfeabcb8fb38075bcaf0a
Amon Sawamura <amon.sawamura@gmail.com>
2019-05-25 15:20:05 +0900

    feat(api): set timeout, memory

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,9 @@
 import * as functions from 'firebase-functions';
 import apiApp from './api';
 
-export const api = functions.https.onRequest(apiApp);
+export const api = functions
+  .runWith({
+    timeoutSeconds: 30,
+    memory: '1GB',
+  })
+  .https.onRequest(apiApp);
