f9e2508c13389f325b17dd9a39e6bd27b7cde0ec
Amon Sawamura <amon.sawamura@gmail.com>
2019-06-30 22:24:19 +0900

    feat(BidAlert): add BidAlert sub component

---
+++ src/components/organisms/OfferCard/BidAlert/BidAlert.tsx
@@ -0,0 +1,35 @@
+import { Button, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText } from '@material-ui/core';
+import React from 'react';
+
+export interface BidAlertProps {
+  title: string;
+  price: number;
+  isMaxPrice?: boolean;
+  open: boolean;
+  onApprove: () => void;
+  onCancel: () => void;
+  onClose: () => void;
+}
+
+export const BidAlert: React.FC<BidAlertProps> = ({ title, isMaxPrice = false, price, open, onApprove, onCancel, onClose }) => {
+  const alertTitle = isMaxPrice ? '落札しますか？' : '入札しますか？';
+  const alerText = isMaxPrice ? `${title} を、即決価格 ¥${price.toLocaleString()} で落札します` : `${title} を、¥${price.toLocaleString()} で入札します`;
+  const alertApproveTitle = isMaxPrice ? '落札する' : '入札する';
+
+  return (
+    <Dialog open={open} onClose={onClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+      <DialogTitle id="alert-dialog-title">{alertTitle}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">{alerText}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel} color="primary">
+          キャンセル
+        </Button>
+        <Button onClick={onApprove} variant="contained" color="primary" autoFocus>
+          {alertApproveTitle}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
