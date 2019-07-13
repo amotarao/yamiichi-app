bd21e06131ce7010b3c1f4ecea40f7a6f2ef22ca
Amon Sawamura <amon.sawamura@gmail.com>
2019-07-13 13:58:46 +0900

    feat(updateOffer): post successBidOffer

--- functions/src/firestore/updateOffer.ts
+++ functions/src/firestore/updateOffer.ts
@@ -4,23 +4,22 @@ import * as _ from 'lodash';
 import { OfferItemDataInterface, TeamItemDataInterface } from '../utils/interfaces';
 import { updateOffer, postBidOffer } from '../utils/slack';
 
-export default async (change: functions.Change<FirebaseFirestore.DocumentSnapshot>) => {
-  const afterData = change.after.data() as OfferItemDataInterface;
-  const beforeData = change.before.data() as OfferItemDataInterface;
+export default async ({ after, before }: functions.Change<FirebaseFirestore.DocumentSnapshot>) => {
+  const afterData = after.data() as OfferItemDataInterface;
+  const beforeData = before.data() as OfferItemDataInterface;
 
   const diff = _.omitBy(afterData, (value, key) => beforeData[key as keyof OfferItemDataInterface] === value);
 
   if ('currentPrice' in diff) {
     if (afterData.maxPrice >= 0 && afterData.maxPrice <= afterData.currentPrice) {
-      await change.after.ref.set(
+      await after.ref.set(
         {
           active: false,
         },
         { merge: true }
       );
-      return;
-    } else {
-      const originalPost = await change.after.ref
+
+      const originalPost = await after.ref
         .collection('posts')
         .where('type', '==', 'createOffer')
         .where('service', '==', 'slack')
@@ -37,7 +36,51 @@ export default async (change: functions.Change<FirebaseFirestore.DocumentSnapsho
       const updateOfferPromise = updateOffer(client, {
         channel: slackDefaultChannel,
         ts: originalTs,
-        id: change.after.id,
+        id: after.id,
+        isFinished: true,
+        item: afterData,
+      });
+
+      const postBidOfferPromise = postBidOffer(client, {
+        channel: slackDefaultChannel,
+        thread_ts: originalTs,
+        isFinished: true,
+        item: afterData,
+      });
+
+      const [{ ok, channel = null, ts = null, ...postResult }] = await Promise.all([updateOfferPromise, postBidOfferPromise]);
+      console.log({ ok, channel, ts, ...postResult });
+
+      await after.ref.collection('posts').add({
+        type: 'successBidOffer',
+        service: 'slack',
+        slack: {
+          ok,
+          channel,
+          ts,
+        },
+      });
+
+      return;
+    } else {
+      const originalPost = await after.ref
+        .collection('posts')
+        .where('type', '==', 'createOffer')
+        .where('service', '==', 'slack')
+        .limit(1)
+        .get();
+      const {
+        slack: { ts: originalTs },
+      } = originalPost.docs[0].data();
+
+      const team = await afterData.teamRef.get();
+      const { slackBotAccessToken, slackDefaultChannel } = team.data() as TeamItemDataInterface;
+      const client = new WebClient(slackBotAccessToken);
+
+      const updateOfferPromise = updateOffer(client, {
+        channel: slackDefaultChannel,
+        ts: originalTs,
+        id: after.id,
         item: afterData,
       });
 
@@ -50,7 +93,7 @@ export default async (change: functions.Change<FirebaseFirestore.DocumentSnapsho
       const [{ ok, channel = null, ts = null, ...postResult }] = await Promise.all([updateOfferPromise, postBidOfferPromise]);
       console.log({ ok, channel, ts, ...postResult });
 
-      await change.after.ref.collection('posts').add({
+      await after.ref.collection('posts').add({
         type: 'updateOffer',
         service: 'slack',
         slack: {
